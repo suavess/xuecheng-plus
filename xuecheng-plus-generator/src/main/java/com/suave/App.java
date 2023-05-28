237e9dbad2a6f0d010e7fcff3f49e54c0758171f
Suave <1132888093@qq.com>
2023-05-28 15:48:33 +0800

    feat:sql及代码生成器

---
+++ xuecheng-plus-generator/src/main/java/com/suave/App.java
@@ -0,0 +1,66 @@
+package com.suave;
+
+import com.baomidou.mybatisplus.generator.FastAutoGenerator;
+import com.baomidou.mybatisplus.generator.config.OutputFile;
+import com.baomidou.mybatisplus.generator.config.rules.DbColumnType;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+
+import java.sql.Types;
+import java.util.Arrays;
+import java.util.Collections;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        FastAutoGenerator.create("jdbc:mysql://127.0.0.1:3306/xcp_content", "root", "123456")
+                .globalConfig(builder -> {
+                    builder.author("Suave") // 设置作者
+                            .enableSwagger() // 开启 swagger 模式
+                            .outputDir(System.getProperty("user.dir") + "/xuecheng-plus-generator/src/main/java")
+                            .fileOverride();
+                })
+                .dataSourceConfig(builder -> builder.typeConvertHandler((globalConfig, typeRegistry, metaInfo) -> {
+                    int typeCode = metaInfo.getJdbcType().TYPE_CODE;
+                    if (typeCode == Types.SMALLINT) {
+                        // 自定义类型转换
+                        return DbColumnType.INTEGER;
+                    }
+                    return typeRegistry.getColumnType(metaInfo);
+
+                }))
+                .packageConfig(builder -> {
+                    builder.parent("com.suave") // 设置父包名
+                            .moduleName("") // 设置父包模块名
+                            .pathInfo(Collections.singletonMap(OutputFile.xml, System.getProperty("user.dir") + "/xuecheng-plus-generator/src/main/resources/mapper")); // 设置mapperXml生成路径
+                })
+                .strategyConfig(builder -> {
+                    // 设置需要生成的表名
+                    builder.addInclude(Arrays.asList(
+                                    "course_audit",
+                                    "course_base",
+                                    "course_category",
+                                    "course_market",
+                                    "course_publish",
+                                    "course_publish_pre",
+                                    "course_teacher",
+                                    "mq_message",
+                                    "mq_message_history",
+                                    "teachplan",
+                                    "teachplan_media",
+                                    "teachplan_work"
+                            ))
+                            .addTablePrefix("")
+                            // 实体类配置
+                            .entityBuilder()
+                            .enableLombok()
+                            .controllerBuilder()
+                            .enableRestStyle()
+                            .enableFileOverride();
+                })
+                // 使用Freemarker引擎模板
+                .templateEngine(new FreemarkerTemplateEngine())
+                .execute();
+    }
+}
