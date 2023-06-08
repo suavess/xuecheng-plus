1608895da80f98ef04b3ddc0d6087cd5c1e56e51
Suave <1132888093@qq.com>
2023-06-08 22:45:57 +0800

    feat:nacos配置

--- xuecheng-plus-content/src/main/java/com/suave/content/ContentApplication.java
+++ xuecheng-plus-content/src/main/java/com/suave/content/ContentApplication.java
@@ -2,11 +2,13 @@ package com.suave.content;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Hello world!
  */
+@EnableDiscoveryClient
 @ComponentScan("com.suave")
 @SpringBootApplication
 public class ContentApplication {
